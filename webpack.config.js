aa8260b3e84c5139b8f8c7ef903c645d42608ade
BugCreators <502461775@qq.com>
2020-09-21 15:00:39 +0800

    update: 添加css压缩

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const dir = (...args) => path.resolve(__dirname, ...args);
 
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 const isEnvProduction = process.env.NODE_ENV === "production";
@@ -76,6 +77,7 @@ module.exports = {
           },
         },
       }),
+      new OptimizeCssAssetsPlugin(),
     ],
   },
   plugins: plugins,
